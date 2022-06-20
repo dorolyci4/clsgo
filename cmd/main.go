5c3d8cc817317b2ed54392290a562da8febef4aa
lovelace <lovelacelee@gmail.com>
2022-06-20 16:36:42 +0800

    :boom:update

--- cmd/main.go
+++ cmd/main.go
@@ -1,61 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"sync"
 	"time"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	. "github.com/lovelacelee/clsgo/pkg/log"
-	. "github.com/lovelacelee/clsgo/pkg/utils"
+	"github.com/lovelacelee/clsgo/pkg/log"
 )
 
 func main() {
 
-	author := &object.Signature{
-		Name:  "lovelacelee",
-		Email: "lovelaelee@gmail.com",
-		When:  time.Now(),
-	}
-	directory, _ := os.Getwd()
+	var workGroup sync.WaitGroup
+	workGroup.Add(1)
 
-	// Opens an already existing repository.
-	r, err := git.PlainOpen(directory)
-	ExitIfError(err)
-	Info("Worktree: %s\n", directory)
-	w, err := r.Worktree()
-	ExitIfError(err)
-
-	// Verify the current status of the worktree using the method Status.
-	Info("git status --porcelain")
-	status, err := w.Status()
-	ExitIfError(err)
-
-	fmt.Println(status)
-
-	Info("git add .")
-	_, err = w.Add(".")
-	ExitIfError(err)
-
-	// Commits the current staging area to the repository.
-	// We should provide the object.Signature of Author of the
-	// commit Since version 5.0.1, we can omit the Author signature, being read
-	// from the git config files.
-	Info("git commit")
-	commit, err := w.Commit("Self committed", &git.CommitOptions{
-		Author: author,
+	log.NewGLC(log.InitOption{
+		Path:     "logs/",
+		Prefix:   "backend",
+		Interval: time.Duration(time.Second * 30),
+		Reserve:  time.Duration(time.Hour * 12),
 	})
-	ExitIfError(err)
 
-	// Prints the current HEAD to verify that all worked well.
-	Info("git show -s")
-	obj, err := r.CommitObject(commit)
-	ExitIfError(err)
-
-	fmt.Println(obj)
-
-	// push using default options
-	err = r.Push(&git.PushOptions{})
-	ExitIfError(err)
+	workGroup.Wait()
 }
--- pkg/utils/dir.go
+++ pkg/utils/dir.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
 )
@@ -47,10 +48,138 @@ func WalkDir(dirPth, suffix string) (dirs []string, files []string, err error) {
 	return dirs, files, err
 }
 
+// Get all files under the specified path, search only the current path,
+// do not enter the next level of directory, support suffix filtering (suffix
+// is empty, not filtered)
+func ListDir(dir, suffix string) (files []string, err error) {
+	files = []string{}
+
+	_dir, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	suffix = strings.ToLower(suffix) //suffix filtering
+
+	for _, _file := range _dir {
+		if _file.IsDir() {
+			continue //
+		}
+		if len(suffix) == 0 || strings.HasSuffix(strings.ToLower(_file.Name()), suffix) {
+			//
+			files = append(files, path.Join(dir, _file.Name()))
+		}
+	}
+
+	return files, nil
+}
+
+func CopyDir(srcPath, desPath string) error {
+
+	if srcInfo, err := os.Stat(srcPath); err != nil {
+		return err
+	} else {
+		if !srcInfo.IsDir() {
+			return errors.New("srcPath is not a valid directory path!")
+		}
+	}
+
+	if desInfo, err := os.Stat(desPath); err != nil {
+		return err
+	} else {
+		if !desInfo.IsDir() {
+			return errors.New("destPath is not a valid directory path!")
+		}
+	}
+
+	if strings.TrimSpace(srcPath) == strings.TrimSpace(desPath) {
+		return errors.New("destPath must be different from srcPath !")
+	}
+
+	err := filepath.Walk(srcPath, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+
+		if path == srcPath {
+			return nil
+		}
+
+		destNewPath := strings.Replace(path, srcPath, desPath, -1)
+
+		if !f.IsDir() {
+			CopyFile(path, destNewPath)
+		} else {
+			if !FileIsExisted(destNewPath) {
+				return MakeDir(destNewPath)
+			}
+		}
+
+		return nil
+	})
+
+	return err
+}
+
+func CopyFile(src, des string) (written int64, err error) {
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	fi, _ := srcFile.Stat()
+	perm := fi.Mode()
+	srcFile.Close()
+
+	input, err := ioutil.ReadFile(src)
+	if err != nil {
+		return 0, err
+	}
+
+	err = ioutil.WriteFile(des, input, perm)
+	if err != nil {
+		return 0, err
+	}
+
+	return int64(len(input)), nil
+}
+
+func MakeDir(dir string) error {
+	if !FileIsExisted(dir) {
+		if err := os.MkdirAll(dir, 0777); err != nil { //os.ModePerm
+			fmt.Println("MakeDir failed:", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func FileIsExisted(filename string) bool {
+	existed := true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		existed = false
+	}
+	return existed
+}
+
+func IsDir(name string) bool {
+	if info, err := os.Stat(name); err == nil {
+		return info.IsDir()
+	}
+	return false
+}
+
 func RunInDir(dir string, cmd *exec.Cmd) (err error) {
 	os.Chdir(dir)
 	if err := cmd.Run(); err != nil {
 		return err
 	}
 	return nil
+}
+
+func DeleteThingsInDir(targetDir string) error {
+	dir, err := ioutil.ReadDir(targetDir)
+	for _, d := range dir {
+		os.RemoveAll(path.Join([]string{targetDir, d.Name()}...))
+	}
 }
